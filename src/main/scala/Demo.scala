9a1fbf7a2c0b504ef43ded0a36bcdaaa5ac0db06
JorgeGarcia20 <alberto-garcia20@outlook.com>
2023-04-04 15:50:40 -0600

    Primer ejercicio del libro learning spark, jugando con del dataset de MnMs

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,9 +1,59 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
-class Demo {
-  def main(main: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Demo").setMaster("local[1]")
-    val sc = SparkContext.getOrCreate(conf)
-    val path = "src/data/penguins.csv"
+object Demo {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("ScalaMnMCount")
+      .config("spark.master", "local")
+      .getOrCreate()
+  /*
+    if(args.length < 1){
+      print("Usage: MnMCount src/data/mnm_dataset.csv")
+
+      sys.exit(1)
+    }
+    val mnmFile = args(0)
+    println(mnmFile)
+
+   */
+
+    val mnmFilePath = "src/data/mnm_dataset.csv"
+
+    val mnmDF = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(mnmFilePath)
+
+    mnmDF.printSchema()
+
+    mnmDF.show(10, false)
+    // Columns State Color Total
+    val query1 = mnmDF
+      .select("State", "Color", "Count")
+      .where(col("Count") > 60)
+      .orderBy("Count")
+    query1.show()
+
+    val countMnMDf = mnmDF
+      .select("State", "Color", "Count")
+      .groupBy("State", "Color")
+      .agg(count("Count").alias("Total"))
+      .orderBy(desc("Total"))
+
+    countMnMDf.show()
+    println(s"Total rows: ${countMnMDf.count()}")
+
+    val CACountMnMDF = mnmDF
+      .select("State", "Color", "Count")
+      .where(col("State") === "CA")
+      .groupBy("State", "Color")
+      .agg(count("Count").alias("Total"))
+      .orderBy(desc("Total"))
+
+    CACountMnMDF.show(10)
+
+    spark.close()
   }
 }
